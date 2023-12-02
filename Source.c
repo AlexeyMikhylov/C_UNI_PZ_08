9187f44c54d9b8dcc120d9ccd0e8b9fb5ce41a69
AlexeyMikhylov <alexeymikhylov02@gmail.com>
2023-12-03 02:45:01 +0300

    ..

--- Source.c
+++ Source.c
@@ -18,6 +18,8 @@ int run(void)
 		exit(1);
 	case 1:
 		task1(); printf("\n"); run();
+	case 2:
+		task2(); printf("\n"); run();
 	case 11:
 		task1_1(); printf("\n"); run();
 	default:
@@ -55,8 +57,8 @@ int task1_1(void)
 	getch();
 }
 
-#define MI1 4
-#define MI2 6
+#define MI1 3
+#define MI2 3
 int task1(void)
 {
 	int M[MI1][MI2];
@@ -94,4 +96,51 @@ int task1(void)
 	}
 
 	printf("min sum = %d", minSum); printf("\n");
+}
+
+int task2(void)
+{
+	int M[MI1][MI2];
+	int total = 0, closest = 0, closestElt;
+	float average;
+	int i, j, x = 0, y = 0;
+
+	for (i = 0; i < MI1; i++) {
+		for (j = 0; j < MI2; j++) {
+			M[i][j] = rand() % 10; // % 10 - from 0 to 10
+		}
+	}
+	closestElt = M[0][0];
+
+	for (i = 0; i < MI1; i++) {
+		for (j = 0; j < MI2; j++) {
+			printf("\t%d", M[i][j]);
+		}
+		printf("\n");
+	}
+
+	for (i = 0; i < MI1; i++) {
+		for (j = 0; j < MI2; j++) {
+			total += M[i][j];
+		}
+	}
+
+	printf("total = %d", total); printf("\n");
+
+	average = ((float)total / (MI1 * MI2));
+	printf("avg = %.2lf", average); printf("\n");
+
+	closest = abs((average - M[0][0]));
+
+	for (i = 0; i < MI1; i++) {
+		for (j = 0; j < MI2; j++) {
+			if (closest > abs((average - M[i][j]))) {
+				closest = abs((average - M[i][j]));
+				closestElt = M[i][j];
+				x = i; y = j;
+			}
+		}
+	}
+
+	printf("closest to average is M[%d][%d] = %d", x, y, closestElt);
 }
